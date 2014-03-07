d0cc8619d8a7589d57649fabab371d6dbef4df60
Earl Carlson <theearlcarlson@gmail.com>
2014-03-07 11:17:58 -0800

    setting it up

---
+++ js/app.js
@@ -0,0 +1,21 @@
+$(document).foundation();
+$(function(){ 
+
+	console.log($('#fixie').height());
+	var height = $('#fixie').height();
+	$('#fixie .columns').css({'height':height});
+	$('#fixie .red').css({'height':height});
+
+$(window).resize(function() {
+
+	var height = $('#fixie').height();
+	$('#fixie .columns').css({'height':height});
+	$('#fixie .red').css({'height':height});
+});
+$('#fixie').toggle();
+
+		$('#grid-button').click(function(e){
+			$('#fixie').fadeToggle();
+			e.preventDefault();
+			});
+	});
